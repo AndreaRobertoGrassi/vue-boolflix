64e3fb0f7673776a50bd465044d988c7d4ecd472
Andrea <grassiandrea95@gmail.com>
2020-12-02 23:20:09 +0100

    milestone 1 e 2

--- js/script.js
+++ js/script.js
@@ -3,29 +3,43 @@ var app =new Vue({
   data:{
     apiKey:'8f23d254d7ec1835913633d0b0a45c83',
     search:'',
-    bandieraImg:'',
-    bandieraArray:[],
+    bandieraFilmsArray:[],
+    bandieraSerieTvArray:[],
     filmsArray:[],
+    serieTvArray:[]
   },
   methods:{
     //funzione per cercare i film
     ricerca:function () {
+
+      //cerco i film
+      this.bandieraFilmsArray=[];   //array per le bandiere
       axios.get('https://api.themoviedb.org/3/search/movie?api_key='+this.apiKey+'&language=it-IT&query='+this.search).then(response => {
         this.filmsArray = response.data.results; //i film risultati dalla ricerca li inserisco nel mio filmsArray
         this.filmsArray.forEach((item) => { //trasformo il voto in base 10, in base 5
           item.vote_average=Math.ceil(item.vote_average/2);
         });
         this.filmsArray.forEach((item) => {
-          if (!this.bandieraArray.includes(item.original_language)) {
-            this.bandieraArray.push('img/'+item.original_language+'.svg');
-          }
+          this.bandieraFilmsArray.push('img/'+item.original_language+'.svg');
         });
-        console.log(this.bandieraArray);
+      });
+
+      //cerco le serie tv
+      this.bandieraSerieTvArray=[];   //array per le bandiere
+      axios.get('https://api.themoviedb.org/3/search/tv?api_key='+this.apiKey+'&language=it-IT&query='+this.search).then(response => {
+        this.serieTvArray = response.data.results; //i film risultati dalla ricerca li inserisco nel mio serieTvArray
+        this.serieTvArray.forEach((item) => { //trasformo il voto in base 10, in base 5
+          item.vote_average=Math.ceil(item.vote_average/2);
+        });
+        this.serieTvArray.forEach((item) => { //trasformo la lingua originale in bandierina
+          this.bandieraSerieTvArray.push('img/'+item.original_language+'.svg');
+        });
       });
     },
 
+    //funzione per bandiera non europea
     setAltImg:function (event) {
-      event.target.src = "img/world.jpg"
+      event.target.src = "img/world.svg"
     }
   }
 })
