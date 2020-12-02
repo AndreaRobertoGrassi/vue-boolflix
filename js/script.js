67a86f7c48c3804e7d54b43e85d2035fd4623a4d
Andrea <grassiandrea95@gmail.com>
2020-12-03 00:06:07 +0100

    milestone 3

--- js/script.js
+++ js/script.js
@@ -3,37 +3,50 @@ var app =new Vue({
   data:{
     apiKey:'8f23d254d7ec1835913633d0b0a45c83',
     search:'',
-    bandieraFilmsArray:[],
-    bandieraSerieTvArray:[],
-    filmsArray:[],
-    serieTvArray:[]
+    bandieraFilmsArray:[],  //bandiere per film
+    bandieraSerieTvArray:[],  //bandiere per serietv
+    filmsArray:[],    //films
+    serieTvArray:[],   //serie tv
+    copertinaFilmsArray:[],   //copertine per film
+    copertinaSerieTvArray:[]   //copertine per serie tv
   },
   methods:{
-    //funzione per cercare i film
+
+    //funzione di ricerca
     ricerca:function () {
 
       //cerco i film
-      this.bandieraFilmsArray=[];   //array per le bandiere
       axios.get('https://api.themoviedb.org/3/search/movie?api_key='+this.apiKey+'&language=it-IT&query='+this.search).then(response => {
         this.filmsArray = response.data.results; //i film risultati dalla ricerca li inserisco nel mio filmsArray
-        this.filmsArray.forEach((item) => { //trasformo il voto in base 10, in base 5
-          item.vote_average=Math.ceil(item.vote_average/2);
-        });
-        this.filmsArray.forEach((item) => {
-          this.bandieraFilmsArray.push('img/'+item.original_language+'.svg');
-        });
+        this.fnFilmsArray();
       });
 
       //cerco le serie tv
-      this.bandieraSerieTvArray=[];   //array per le bandiere
       axios.get('https://api.themoviedb.org/3/search/tv?api_key='+this.apiKey+'&language=it-IT&query='+this.search).then(response => {
         this.serieTvArray = response.data.results; //i film risultati dalla ricerca li inserisco nel mio serieTvArray
-        this.serieTvArray.forEach((item) => { //trasformo il voto in base 10, in base 5
-          item.vote_average=Math.ceil(item.vote_average/2);
-        });
-        this.serieTvArray.forEach((item) => { //trasformo la lingua originale in bandierina
-          this.bandieraSerieTvArray.push('img/'+item.original_language+'.svg');
-        });
+        this.fnSerieTvArray();
+      });
+    },
+
+    //funzione che riguardai film per inserire media voto in base 5, la copertina e la bandiera 
+    fnFilmsArray:function () {
+      this.bandieraFilmsArray=[];   //per le bandiere
+      this.copertinaFilmsArray=[];   //per le copertine
+      this.filmsArray.forEach(item => {
+        item.vote_average=Math.ceil(item.vote_average/2);    //trasformo il voto in base 10, in base 5
+        this.copertinaFilmsArray.push('https://image.tmdb.org/t/p/w154'+item.poster_path);  //inserisco l'immagine di copertina
+        this.bandieraFilmsArray.push('img/'+item.original_language+'.svg');   //inserisco la bandiera in base alla lingua
+      });
+    },
+
+    //funzione che riguardai film per inserire media voto in base 5, la copertina e la bandiera
+    fnSerieTvArray:function () {
+      this.bandieraSerieTvArray=[];   //array per le bandiere
+      this.copertinaSerieTvArray=[];   //per le copertine
+      this.serieTvArray.forEach(item => {
+        item.vote_average=Math.ceil(item.vote_average/2);   //trasformo il voto in base 10, in base 5
+        this.copertinaSerieTvArray.push('https://image.tmdb.org/t/p/w154'+item.poster_path);   //inserisco l'immagine di copertina
+        this.bandieraSerieTvArray.push('img/'+item.original_language+'.svg');   //inserisco la bandiera in base alla lingua
       });
     },
 
