ab1596786d5a09bce85fc220a33693dc83fe2524
Andrea <grassiandrea95@gmail.com>
2020-12-04 12:52:48 +0100

    aggiunta del cast

--- js/script.js
+++ js/script.js
@@ -3,12 +3,11 @@ var app =new Vue({
   data:{
     apiKey:'8f23d254d7ec1835913633d0b0a45c83',
     search:'',  //input di ricerca
-    bandieraFilmsArray:[],  //bandiere per film
-    bandieraSerieTvArray:[],  //bandiere per serietv
-    filmsArray:[],    //films
-    serieTvArray:[],   //serie tv
-    copertinaFilmsArray:[],   //copertine per film
-    copertinaSerieTvArray:[]   //copertine per serie tv
+    display:'block',
+    ricercaArray:[],
+    bandieraArray:[],
+    copertinaArray:[],
+    castArray:[]
   },
   mounted:function () {
     //focus automatico sull'input
@@ -19,39 +18,49 @@ var app =new Vue({
 
     //funzione di ricerca
     ricerca:function () {
-
+      this.ricercaArray=[];
+      this.copertinaArray=[];
+      this.bandieraArray=[];
+      this.castArray=[];
       //cerco i film
-      axios.get('https://api.themoviedb.org/3/search/movie?api_key='+this.apiKey+'&language=it-IT&query='+this.search).then(response => {
-        this.filmsArray = response.data.results; //i film risultati dalla ricerca li inserisco nel mio filmsArray
-        this.fnFilmsArray();
-      });
+      axios.get('https://api.themoviedb.org/3/search/multi?api_key='+this.apiKey+'&language=it-IT&sort_by=popularity.desc&query='+this.search).then(response => {
+        this.ricercaArray=response.data.results;
+        this.ricercaArray.forEach((item, i) => {
+          //se film
+          if (item.media_type=='movie') {
+            axios.get('https://api.themoviedb.org/3/movie/'+item.id+'/credits?api_key='+this.apiKey+'&language=it-IT').then(response => {
+              if (response.data.cast.length>0) {
+                let nuovoCast={
+                  attori1:response.data.cast[0].original_name,
+                  attori2:response.data.cast[1].original_name,
+                  attore3:response.data.cast[2].original_name
+                }
+                this.castArray.push(nuovoCast);
+              }else {
+                this.castArray.push({nessuno:'Non disponibile'})
+              }
+            });
+          }else { //se serie tv
+            axios.get('https://api.themoviedb.org/3/tv/'+item.id+'/credits?api_key='+this.apiKey+'&language=it-IT').then(response => {
+              if (response.data.cast.length>0) {
+                let nuovoCast={
+                  attori1:response.data.cast[0].original_name,
+                  attori2:response.data.cast[1].original_name,
+                  attori3:response.data.cast[2].original_name
+                }
+                this.castArray.push(nuovoCast);
+              }else {
+                this.castArray.push({nessuno:'Non disponibile'})
+              }
+            });
+          }
+          console.log(this.castArray);
+          item.vote_average=Math.ceil(item.vote_average/2);    //trasformo il voto in base 10, in base 5
 
-      //cerco le serie tv
-      axios.get('https://api.themoviedb.org/3/search/tv?api_key='+this.apiKey+'&language=it-IT&query='+this.search).then(response => {
-        this.serieTvArray = response.data.results; //i film risultati dalla ricerca li inserisco nel mio serieTvArray
-        this.fnSerieTvArray();
-      });
-    },
+          this.copertinaArray.push('https://image.tmdb.org/t/p/w342/'+item.poster_path);  //inserisco l'immagine di copertina
 
-    //funzione che riguarda i film per inserire media voto in base 5, la copertina e la bandiera
-    fnFilmsArray:function () {
-      this.bandieraFilmsArray=[];   //per le bandiere
-      this.copertinaFilmsArray=[];   //per le copertine
-      this.filmsArray.forEach(item => {
-        item.vote_average=Math.ceil(item.vote_average/2);    //trasformo il voto in base 10, in base 5
-        this.copertinaFilmsArray.push('https://image.tmdb.org/t/p/w342/'+item.poster_path);  //inserisco l'immagine di copertina
-        this.bandieraFilmsArray.push('img/'+item.original_language+'.svg');   //inserisco la bandiera in base alla lingua
-      });
-    },
-
-    //funzione che riguarda i film per inserire media voto in base 5, la copertina e la bandiera
-    fnSerieTvArray:function () {
-      this.bandieraSerieTvArray=[];   //array per le bandiere
-      this.copertinaSerieTvArray=[];   //per le copertine
-      this.serieTvArray.forEach(item => {
-        item.vote_average=Math.ceil(item.vote_average/2);   //trasformo il voto in base 10, in base 5
-        this.copertinaSerieTvArray.push('https://image.tmdb.org/t/p/w342/'+item.poster_path);   //inserisco l'immagine di copertina
-        this.bandieraSerieTvArray.push('img/'+item.original_language+'.svg');   //inserisco la bandiera in base alla lingua
+          this.bandieraArray.push('img/'+item.original_language+'.svg');   //inserisco la bandiera in base alla lingua
+        });
       });
     },
 
