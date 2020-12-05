445d381bf5a11a54d2a8ade179d57e9fb5736474
Andrea <grassiandrea95@gmail.com>
2020-12-05 16:57:26 +0100

    al caricamento pagina vedo i film popolari in ordine desc

--- js/script.js
+++ js/script.js
@@ -6,11 +6,27 @@ var app =new Vue({
     ricercaArray:[],
     bandieraArray:[],
     copertinaArray:[],
-    castArray:[]
+    castArray:[],
+    popularArray:[],
+    displayCast:false   // rende invisibile la parola cast al caricamento della pagina per film popolari
   },
+
   mounted:function () {
     //focus automatico sull'input
     document.getElementById('input').focus();
+
+    // al caricamento inserisco in oagina i film piu popolari
+    axios.get('https://api.themoviedb.org/3/discover/movie?api_key='+this.apiKey+'&language=it-IT&sort_by=popularity.desc').then(response =>{
+      this.popularArray=response.data.results;
+      axios.get('https://api.themoviedb.org/3/discover/tv?api_key='+this.apiKey+'&language=it-IT&sort_by=popularity.desc').then(response =>{
+        this.ricercaArray=[...this.popularArray,...response.data.results];
+        this.ricercaArray.forEach((item, i) => {
+          item.vote_average=Math.ceil(item.vote_average/2);    //trasformo il voto in base 10, in base 5
+          this.copertinaArray.push('https://image.tmdb.org/t/p/w342/'+item.poster_path);  //inserisco l'immagine di copertina
+          this.bandieraArray.push('img/'+item.original_language+'.svg');   //inserisco la bandiera in base alla lingua
+        });
+      });
+    });
   },
 
   methods:{
@@ -21,7 +37,9 @@ var app =new Vue({
       this.copertinaArray=[];
       this.bandieraArray=[];
       this.castArray=[];
-      //cerco i film
+      this.displayCast=true;
+
+      //cerco i film/serie tv
       axios.get('https://api.themoviedb.org/3/search/multi?api_key='+this.apiKey+'&language=it-IT&sort_by=popularity.desc&query='+this.search).then(response => {
         this.ricercaArray=response.data.results;
         this.ricercaArray.forEach((item, i) => {   //elimino gli elementi senza copertina
@@ -33,29 +51,11 @@ var app =new Vue({
           //se film
           if (item.media_type=='movie') {
             axios.get('https://api.themoviedb.org/3/movie/'+item.id+'/credits?api_key='+this.apiKey+'&language=it-IT').then(response => {
-              if (response.data.cast.length>0) {
-                let nuovoCast={
-                  attori1:response.data.cast[0].original_name,
-                  attori2:response.data.cast[1].original_name,
-                  attore3:response.data.cast[2].original_name
-                }
-                this.castArray.push(nuovoCast);
-              }else {
-                this.castArray.push({nessuno:'Non disponibile'})
-              }
+              this.inserimentoCast(response);
             });
           }else { //se serie tv
             axios.get('https://api.themoviedb.org/3/tv/'+item.id+'/credits?api_key='+this.apiKey+'&language=it-IT').then(response => {
-              if (response.data.cast.length>0) {
-                let nuovoCast={
-                  attori1:response.data.cast[0].original_name,
-                  attori2:response.data.cast[1].original_name,
-                  attori3:response.data.cast[2].original_name
-                }
-                this.castArray.push(nuovoCast);
-              }else {
-                this.castArray.push({nessuno:'Non disponibile'})
-              }
+              this.inserimentoCast(response);
             });
           }
 
@@ -66,9 +66,22 @@ var app =new Vue({
       });
     },
 
+    inserimentoCast:function (response) {
+      if (response.data.cast.length>0) {
+        let nuovoCast={
+          attoriUno:response.data.cast[0].original_name,
+          attoriDue:response.data.cast[1].original_name,
+          attoreTre:response.data.cast[2].original_name
+        }
+        this.castArray.push(nuovoCast);
+      }else {
+        this.castArray.push({nessuno:'Non disponibile'})
+      }
+    },
+
     //funzione per bandiera non europea
     setAltImgBandiera:function (event) {
       event.target.src = "img/world.svg"
     },
   }
-})
+});
